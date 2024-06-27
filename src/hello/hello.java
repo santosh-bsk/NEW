e2f87bbd75636dbd7cec460cd35f66b801a7236b
santosh-bsk <bsantosh.kgl@gmail.com>
2024-06-27 12:33:53 +0530

    first

---
+++ src/hello/hello.java
@@ -0,0 +1,18 @@
+package hello;
+
+import java.util.Scanner;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter string:");
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		
+		
+		System.out.println("Hi Mrs. "+s);
+
+	}
+
+}
